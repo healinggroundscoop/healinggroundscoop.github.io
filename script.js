5e67e253d832a5a2c43b7b4a4b07060aef2f6307
xIdentified <73047386+xIdentified@users.noreply.github.com>
2024-03-25 20:58:55 -0400

    ok

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-// Example: FAQ toggle
-document.addEventListener('DOMContentLoaded', (event) => {
-    const faqHeaders = document.querySelectorAll('.faq-header');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    faqHeaders.forEach(header => {
-        header.addEventListener('click', function() {
-            this.nextElementSibling.classList.toggle('active');
-        });
-    });
-});
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide")[0].getElementsByTagName("img");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
